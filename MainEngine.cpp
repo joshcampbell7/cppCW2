64208edb0635e732fbab79c3f57f2b81e4a4b902
Josh Wilkins <joshuawilkins1845@gmail.com>
2023-12-01 20:57:43 +0000

    implemented goMethod

--- MainEngine.cpp
+++ MainEngine.cpp
@@ -9,39 +9,54 @@
 #include "MainEngine.h"
 #include "Player.h"
 #include "jsonParser.h"
+
 using namespace std;
 
-Player player = Player("player1","player","room1");
+Player player = Player("player1", "player", "room1");
+bool newRoom = true;
 
-string goMethod(const vector<string>& command){
-    return "This is the go method.";
+string goMethod(const vector<string> &command) {
+    if (getRooms().at(player.getCurrentRoomId()).getExits().count(command.at(1))) {
+        player.setCurrentRoom(getRooms().at(player.getCurrentRoomId()).getExits().at(command.at(1)));
+        newRoom = true;
+        return "Travelling " + command.at(1) + "...";
+    }
+    newRoom = false;
+    return "Please enter a valid direction.";
 }
 
-string takeMethod(const vector<string>& command){
+string takeMethod(const vector<string> &command) {
     return "This is the take method.";
 }
 
-string lookMethod(const vector<string>& command){
-    if (find(getRooms().at(player.getCurrentRoomId()).getObjects().begin(), getRooms().at(player.getCurrentRoomId()).getObjects().end(), command.at(1)) != getRooms().at(player.getCurrentRoomId()).getObjects().end()){
+string lookMethod(const vector<string> &command) {
+    if (find(getRooms().at(player.getCurrentRoomId()).getObjects().begin(),
+             getRooms().at(player.getCurrentRoomId()).getObjects().end(), command.at(1)) !=
+        getRooms().at(player.getCurrentRoomId()).getObjects().end()) {
         //return getObjects().at(command.at(1));
     }
     return "That object is not in this room...";
 }
 
-string fightMethod(const vector<string>& command){
+string fightMethod(const vector<string> &command) {
     return "This is the fight method.";
 }
 
-string moveHandler(){
+string moveHandler() {
     //enum and map set up to facilitate use of switch case statements as they can't handle strings
-    enum moveCode{mGo,mTake,mLook,mFight};
-    map<string,moveCode> m = {{"look",mLook},{"go",mGo},{"take",mTake},{"fight",mFight}};
+    enum moveCode {
+        mGo, mTake, mLook, mFight
+    };
+    map<string, moveCode> m = {{"look",  mLook},
+                               {"go",    mGo},
+                               {"take",  mTake},
+                               {"fight", mFight}};
 
     cout << "\nYou have 4 input command options: go, look, take, fight." << endl;
-    bool newRoom = true;
     while (!getObjectives().at("objective1").getWhat().empty()) {
         if (newRoom) {
-            cout << "\nYou are currently in " + player.getCurrentRoomId() + ". " + getRooms().at(player.getCurrentRoomId()).getDescription() << endl;
+            cout << "\nYou are currently in " + player.getCurrentRoomId() + ". " +
+                    getRooms().at(player.getCurrentRoomId()).getDescription() << endl;
             newRoom = false;
         }
         //input stream needs to be cleared before using the getline function
@@ -65,7 +80,6 @@ string moveHandler(){
         switch (m.at(command.at(0))) {
             case mGo:
                 cout << goMethod(command) << endl;
-                newRoom = true;
                 break;
             case mTake:
                 cout << takeMethod(command) << endl;
@@ -84,7 +98,7 @@ string moveHandler(){
     return "";
 }
 
-void mainEngine(){
+void mainEngine() {
     cout << "Welcome to Map .. of Text Adventure Game" << endl;
     cout << "Before we start, What is your name?" << endl;
     string name;
@@ -92,28 +106,30 @@ void mainEngine(){
     player.setName(name);
     player.setCurrentRoom(getInitialRoom());
 
-    cout << "Welcome " + player.getPlayerName() + "!, " + "you have " + to_string(player.getHealth()) + " health to start with. " + "You also have " + to_string(player.getObjects().size()) + " objects in your inventory." << endl;
-    for (const auto& pair : getObjectives()) {
+    cout << "Welcome " + player.getPlayerName() + "!, " + "you have " + to_string(player.getHealth()) +
+            " health to start with. " + "You also have " + to_string(player.getObjects().size()) +
+            " objects in your inventory." << endl;
+    for (const auto &pair: getObjectives()) {
         std::cout << pair.first << std::endl;
     }
 
-    if(getObjectives().at("objective1").getType() == "kill"){
+    if (getObjectives().at("objective1").getType() == "kill") {
         cout << "\nTo win the game, your objective is to kill the following enemies: " << endl;
-        for(int i=0; i<getObjectives().at("objective1").getWhat().size();i++){
+        for (int i = 0; i < getObjectives().at("objective1").getWhat().size(); i++) {
             cout << getObjectives().at("objective1").getWhat().at(i) << endl;
         }
     }
 
-    if(getObjectives().at("objective1").getType() == "collect"){
+    if (getObjectives().at("objective1").getType() == "collect") {
         cout << "\nTo win the game, your objective is to collect the following items: " << endl;
-        for(string i : getObjectives().at("objective1").getWhat()){
+        for (string i: getObjectives().at("objective1").getWhat()) {
             cout << i << endl;
         }
     }
 
-    if(getObjectives().at("objective1").getType() == "room"){
+    if (getObjectives().at("objective1").getType() == "room") {
         cout << "\nTo win the game, your objective is to visit the following rooms: " << endl;
-        for(string i : getObjectives().at("objective1").getWhat()){
+        for (string i: getObjectives().at("objective1").getWhat()) {
             cout << i << endl;
         }
     }
